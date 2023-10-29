b41fc57efb7975102f21c4dcfb5e39c8aab7a988
Changho Youn <younch8342@yu.ac.kr>
2023-10-29 17:09:33 +0900

    model.js 코드수정

--- js/model.js
+++ js/model.js
@@ -1,7 +1,10 @@
 
 class Finder {
     constructor() {
-        this.url = "https://api.github.com/users/";
+        this.bind_elements();
+        this.init_form();
+        this.fetchData(this.input_value);
+        this.show_user_info(this.user_data);
 
     }
     
@@ -16,34 +19,40 @@ class Finder {
         this.search_button = document.querySelector('.search_button');
         this.username_inputbox_ele = document.querySelector('#username_inputbox');
     }
-}
 
-
-
-async function fetchData(username) {
-    try {
-        const response = await fetch("https://api.github.com/users/" + username);
-        if (response.statusCode === 404) throw new Error("User not found");
-        user_data = await response.json();
-    } catch (e) {
-        console.log(e);
-        alert("Error fetching data. check username and try");
+    init_form(){
+        this.search_button.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.input_value = this.username_inputbox_ele.value;
+        });
     }
+
+    async fetchData(username) {
+        try {
+            const response = await fetch("https://api.github.com/users/" + username);
+            if (response.statusCode === 404) throw new Error("User not found");
+            this.user_data = await response.json();
+        } catch (e) {
+            console.log(e);
+            alert("Error fetching data. check username and try");
+        }
+    }
+
+    show_user_info(user_data){
+        const user_img = document.createElement('img');
+        user_img.src = user_data.avatar_url;
+        this.avatar_ele.appendChild(user_img); //image 처리
+
+        this.username_ele.textContent = user_data.login;
+        this.followers.textContent = user_data.followers;
+        this.following.textContent = user_data.following;
+        this.repos_cnt_ele.textContent = user_data.public_repos;
+        this.created_at_ele.textContent = user_data.created_at;
+        this.repos_url.href = user_data.html_url;
+        this.username_inputbox_ele.innertText = '';
+    }
+    
 }
-
-
-
-function bind_elements() {
-    username_ele = document.querySelector('.profile_username');
-    avatar_ele = document.querySelector('.profile_image_container');
-    followers = document.querySelector('.profile_followers');
-    following = document.querySelector('#follwing');
-    repos_url = document.querySelector('.latest_repos');
-    repos_cnt_ele = document.querySelector('.repo_count');
-    created_at_ele = document.querySelector('.user_since');
-    search_button = document.querySelector('.search_button');
-    username_inputbox_ele = document.querySelector('#username_inputbox');
-}
-User.init();
+const finder = new Finder();
 
 
